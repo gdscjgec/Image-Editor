453ba0c65bd0e03358cd5a7781f20e648f0cd188
Mitul Jain <mitulgoeljain0123.com>
2022-02-01 00:43:41 +0530

    Added Scroll-to-Top Button

--- js/app.js
+++ js/app.js
@@ -309,3 +309,12 @@ fileBtn.addEventListener('change', function(){
 });
 
 cropButtonDOM.addEventListener("click", cropImage);
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
